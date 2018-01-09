190c555d716a24b1100c4318f0ec1a2e3747201e
chaimard <chaimard@email.krirk.ac.th>
2018-01-09 15:44:09 +0700

    back controller

--- app/src/main/java/chaimard/krirk/ac/th/chaicalculate/fragment/CalculateFragment.java
+++ app/src/main/java/chaimard/krirk/ac/th/chaicalculate/fragment/CalculateFragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
+import chaimard.krirk.ac.th.chaicalculate.MainActivity;
 import chaimard.krirk.ac.th.chaicalculate.R;
 
 /**
@@ -15,6 +17,40 @@ import chaimard.krirk.ac.th.chaicalculate.R;
 // พิมพ์ extends Fragment แล้วให้เอา เคอร์เซอร์ วางภายใน กด alt+Insert  เลือก Override method  แลือก onCreateView
 
 public class CalculateFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create toolbar
+        createToolbar();
+
+
+    }   //Main Method
+
+    private void createToolbar() {
+
+        android.support.v7.widget.Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
